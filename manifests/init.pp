160bddbffc9ac1902b434a5f676ce382b88b9b3d
Ramin Khatibi <ramin.khatibi@gmail.com>
2023-11-02 01:10:12 +0000

    hiera data

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,20 +3,20 @@
 # Installs and configures NSD, the authoritative DNS resolver from NLnet Labs
 #
 class nsd (
-  String $config_d,
+  Stdlib::Absolutepath $config_d,
   String $config_file,
-  String $service_name,
-  Variant[String,Undef] $package_name,
   String $control_cmd,
-  String $zonedir,
-  Boolean $zonepurge = false,
-  String $group,
-  String $owner,
   String $database,
-  Integer $verbosity = 0,
-  Integer $port = 53,
+  Stdlib::Absolutepath $zonedir,
+  String $group = 'nsd',
   Array[Stdlib::Ip::Address] $interface = ['::0','0.0.0.0'],
   Optional[String] $logfile = undef,
+  String $owner = 'nsd',
+  Variant[String,Undef] $package_name = 'nsd',
+  Stdlib::Port $port = 53,
+  String $service_name = 'nsd',
+  Integer $verbosity = 0,
+  Boolean $zonepurge = false,
 ) {
 
   contain nsd::install
