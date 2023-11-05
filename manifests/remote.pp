3b83ca87fc05ee8247ac2785d075fce75a22be14
Ramin Khatibi <ramin.khatibi@gmail.com>
2023-11-05 05:32:41 +0000

    too clever

--- manifests/remote.pp
+++ manifests/remote.pp
@@ -3,20 +3,21 @@
 # Configure remote control of the nsd daemon process
 #
 class nsd::remote (
-  Boolean $enable                                  = true,
-  Array[String] $interface                         = ['::1', '127.0.0.1'],
-  Integer $port                                    = 8952,
-  Optional[Stdlib::Absolutepath] $server_key_file   = undef,
-  Optional[Stdlib::Absolutepath] $server_cert_file  = undef,
-  Optional[Stdlib::Absolutepath] $control_key_file  = undef,
+  Boolean $enable = true,
+  Array[String] $interface = ['::1', '127.0.0.1'],
+  Stdlib::Port $port = 8952,
+  Optional[Stdlib::Absolutepath] $server_key_file = undef,
+  Optional[Stdlib::Absolutepath] $server_cert_file = undef,
+  Optional[Stdlib::Absolutepath] $control_key_file = undef,
   Optional[Stdlib::Absolutepath] $control_cert_file = undef,
 ) {
-  include nsd
 
-  $config_file = $nsd::config_file
+  include nsd
 
   concat::fragment { 'nsd-remote':
     order   => '10',
-    target  => $config_file,
+    target  => $nsd::config_file,
     content => epp('nsd/remote'),
+  }
+
 }
